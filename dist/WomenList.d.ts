f8bf682c2e3548c1c051245920f712dc777253a4
Clémence Starosta <97520424+clemstarosta@users.noreply.github.com>
2022-11-21 10:43:15 +0100

    Delete dist directory

--- dist/WomenList.d.ts
+++
@@ -1,9 +0,0 @@
-import { Woman } from './Woman';
-export declare class WomenList {
-    private storedWomen;
-    addWoman(woman: Woman): void;
-    getWoman(name: string): Woman;
-    getDescriptionOf(name: string): string;
-    getAllWomen(): Woman[];
-    getTotalNumberOfWomen(): number;
-}
