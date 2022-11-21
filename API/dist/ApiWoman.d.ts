b75559bc9565b164294b374a4c8e97319d31eb8b
Clémence Starosta <97520424+clemstarosta@users.noreply.github.com>
2022-11-21 10:54:17 +0100

    Add files via upload

---
+++ API/dist/ApiWoman.d.ts
@@ -0,0 +1,10 @@
+export interface ApiWoman {
+    name: string;
+    desc1: string;
+    desc2: string;
+    desc3: string;
+    desc4: string;
+    desc5: string;
+    place: string;
+    category: string;
+}
