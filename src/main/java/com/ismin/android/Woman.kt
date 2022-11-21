3ea7930d1b4eaa9e174c1901a24720cd53d0a50d
Clémence Starosta <97520424+clemstarosta@users.noreply.github.com>
2022-11-21 14:20:23 +0100

    Add files via upload

---
+++ src/main/java/com/ismin/android/Woman.kt
@@ -0,0 +1,4 @@
+package com.ismin.android
+
+data class Woman(val name: String, val category: String, val place: String, val desc1: String,
+                 val desc2: String, val desc3: String, val desc4: String, val desc5: String): java.io.Serializable
