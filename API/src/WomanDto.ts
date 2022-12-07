1e38fb73a326fdf2332aed99ea8a94ea16a55421
Clémence Starosta <97520424+clemstarosta@users.noreply.github.com>
2022-12-07 09:30:26 +0100

    ajout du favori

--- API/src/WomanDto.ts
+++ API/src/WomanDto.ts
@@ -45,6 +45,7 @@ export class WomanDto {
   @IsString()
   readonly thumb_url : string;
 
+  fav: boolean;
 
 
   constructor(name: string, category: string, place: string, desc1: string, desc2: string, desc3: string, desc4: string, desc5: string, 
